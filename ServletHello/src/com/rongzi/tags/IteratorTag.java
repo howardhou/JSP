cbb63829fcffdf94ffbf090eaf240832303b2e27
houdongdong <houdongdong@rongzi.com>
2018-09-12 20:03:20 +0800

    带标签体的自定义标签 - 迭代标签

---
+++ ServletHello/src/com/rongzi/tags/IteratorTag.java
@@ -0,0 +1,46 @@
+package com.rongzi.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.util.Collection;
+
+public class IteratorTag extends SimpleTagSupport {
+
+    // 指定需要被被迭代的集合
+    private String collection;
+
+    // 指定迭代集合元素
+    private String item;
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public void setCollection(String collection) {
+        this.collection = collection;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+
+        // 获得属性名为collection 的集合
+        Collection itemList = (Collection) getJspContext().getAttribute(collection);
+
+        for (Object s : itemList) {
+            // 将集合元素设置到 page
+            getJspContext().setAttribute(item, s);
+            // getJspBody() 返回自定义标签的标签体 ，JSPFragment 对象
+            // invoke（）输出标签体的内容
+            getJspBody().invoke(null);
+        }
+    }
+}
--- ServletHello/web/index.jsp
+++ ServletHello/web/index.jsp
@@ -1,4 +1,5 @@
-<%--
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %><%--
   Created by IntelliJ IDEA.
   User: dongdong
   Date: 2018/9/7
@@ -17,7 +18,26 @@
   <h3>显示自定义标签内容</h3>
   <mytag:helloWorld/><br>
 
-  <h3>带属性的自定义标签，查询mysql</h3>
+  <h3>带属性的自定义标签，不建议查询mysql，会被调用3次</h3>
   <mytag:query driver="com.mysql.jdbc.Driver" url="jdbc:mysql://localhost:3306/testdb" username="root" password="123456" sql="SELECT * FROM user" />
+
+  <h3>带标签体的自定义标签 - 迭代标签</h3>
+  <%
+    List<String> list = new ArrayList<String>();
+    list.add("疯狂Java");
+    list.add("www.rongzi.com");
+    list.add("java");
+    pageContext.setAttribute("list", list);
+  %>
+
+  <table border="1" bgcolor="#a52a2a" width="300">
+    <mytag:iterator collection="list" item="item">
+      <tr>
+        <td>${pageScope.item}</td>
+      </tr>
+    </mytag:iterator>
+  </table>
+
   </body>
+
 </html>
